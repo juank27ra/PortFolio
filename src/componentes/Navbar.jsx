4c468af33738f3748f8eda1e35785885ed1d46ce
Juan Carloss Ramirez <juank27ra@gmail.com>
2024-03-23 13:25:31 -0500

    modify route nabvar underline

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div>
-      <nav className='w-[100%] h-24 sm:h-auto bg-[#01161E] border-b-2 border-[#eff6e0] flex justify-between shadow-md shadow-[#eff6e0] px-10 sm:py-10 sm:px-0 md:w-auto lg:w-auto sm:flex sm:flex-col sm:gap-4 sm:items-center'>
+      <nav className='w-[100%] h-24 sm:h-auto bg-[#01161E] border-b-2 border-[#eff6e0] flex justify-between shadow-md shadow-[#eff6e0] hover:shadow-md hover:shadow-[#39c8f7] px-10 sm:py-10 sm:px-0 md:w-auto lg:w-auto sm:flex sm:flex-col sm:gap-4 sm:items-center'>
         <Link to={'/'}  className=' p-4'>{<FaHouse className='h-12 w-12 fill-[#eff6e0] my-auto hover:scale-125 hover:fill-[#39c8f7]'/>}</Link> 
-          <Link to='/about' className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto  hover:underline hover:text-[#39c8f7] hover:scale-125 md:hover:scale-110 selection:text-[##39c8f7]'> Sobre mí </Link>  
-          <Link to='/proyects' className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto  hover:underline hover:text-[#39c8f7] hover:scale-125 md:hover:scale-110 selection:text-[##39c8f7]'> Proyectos </Link>  
-          <Link to='/contact' className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto  hover:underline hover:text-[#39c8f7] hover:scale-125 md:hover:scale-110 selection:text-[##39c8f7]'> Contácteme </Link>  
-        <Link to={pdfcv} target='_blank'  className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto shadow-md p-4 shadow-[#eff6e0] hover:underline hover:text-[#39c8f7] selection:text-[##39c8f7] sm:w-[12rem] sm:flex sm:justify-end'> Ver/Descargar cv </Link>
+          <Link to='/about' className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto  hover:text-[#39c8f7] hover:scale-125 md:hover:scale-110 selection:text-[##39c8f7] relative hoverAfterNav'> Sobre mí </Link>  
+          <Link to='/proyects' className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto  hover:text-[#39c8f7] hover:scale-125 md:hover:scale-110 selection:text-[##39c8f7] relative hoverAfterNav'> Proyectos </Link>  
+          <Link to='/contact' className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto  hover:text-[#39c8f7] hover:scale-125 md:hover:scale-110 selection:text-[##39c8f7] relative hoverAfterNav'> Contácteme </Link>  
+        <Link to={pdfcv} target='_blank'  className=' z-10 text-xl md:text-lg font-bold  text-[#eff6e0] my-auto p-4 shadow-md  shadow-[#eff6e0] hover:text-[#39c8f7] selection:text-[#39c8f7] hover:shadow-md  hover:shadow-[#39c8f7] sm:w-[12rem] sm:flex sm:justify-end'> Ver/Descargar cv </Link>
       </nav>
     </div>
   )
--- src/componentes/Profile.jsx
+++ src/componentes/Profile.jsx
@@ -14,6 +14,8 @@ const Profile = () => {
           <h1 className=' text-4xl font-bold text-[#eff6e0]  mb-9 sm:text-2xl'>Ramírez Pinilla</h1>
           <h3 className=' text-3xl font-semibold ml-20 sm:ml-0 text-[#39c8f7] sm:text-xl '> Developer Full Stack </h3>
           <p className='text-xl mt-4 text-[#eff6e0]  w-auto sm:text-base text-justify mr-8 sm:w-5/6 sm:mx-auto 2xl:w-[80%]'>Entusiasta de la tecnología y con disciplina en el aprendizaje me defino. Ver cómo se transforma el código visualmente y sentir la grandeza de lo que hago, me pone en una perspectiva de aprendizaje continuo para SER el mejor. </p>
+          <br/>
+          <p className='text-xl mt-4 text-[#eff6e0]  w-auto sm:text-base text-justify mr-8 sm:w-5/6 sm:mx-auto 2xl:w-[80%]'>Busco oportunidades para aplicar mi experiencia y habilidades en proyectos desafiantes como Desarrollador Frontend, donde pueda seguir creciendo y contribuyendo al éxito del equipo.. </p>
           <p className='text-2xl font-semibold mt-10 text-[#eff6e0]  sm:text-lg sm:items-center'>Bienvenidos. </p>
             <div className='flex items-start sm:flex sm:justify-center mb-24'>
               <Link to='about'><button className='flex duration-1000 snap-start border-[#39c8f7] border-2 rounded-xl bg-transparent mt-16 p-4 font-bold text-[#eff6e0] hover:bg-[#356169] hover:border-[#eff6e0] text-xl sm:text-base sm:mt-12 '>Más sobre mí</button></Link>
