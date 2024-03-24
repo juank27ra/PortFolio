0b81bdc4aaf57e11437ab8b75ed4f884e1e831dd
Juan Carloss Ramirez <juank27ra@gmail.com>
2024-03-23 22:01:55 -0500

    modify icons github

--- src/componentes/Contact.jsx
+++ src/componentes/Contact.jsx
@@ -1,6 +1,6 @@
 import {useState, useRef} from 'react'
 import linkedIn from '../assets/linkedin-icon.svg'
-import github from '../assets/github.svg'
+import Github from '../assets/github.svg'
 import mail from '../assets/google-gmail.svg'
 import whatsapp from '../assets/whatsapp-icon.svg'
 import emailjs from '@emailjs/browser'
@@ -48,7 +48,7 @@ export default function Contact() {
     <div className='bg-gradient-to-tl from-[#3b757f] to-[#01161E] h-screen flex flex-row w-auto sm:flex-col sm:w-auto'>
         <section className=' absolut flex flex-col sm:flex-row sm:h-20 sm:w-[100%] sm:ml-0 justify-center bg-[#01161E] items-center h-full w-32 ml-20 gap-8 md:ml-14'>
           <a href='https://www.linkedin.com/in/juan-carlos-ramirez-pinilla-a8518077/' target='_blanc'><img title='LinkedIn' className='h-12 w-12 sm:h-9 sm:w-9 drop-shadow-2xl hover:shadow-blue-400 hover:h-20 hover:w-20 sm:hover:h-16 sm:hover:w-16' src={linkedIn} alt='icono de linkedIn'/></a> 
-          <a href='https://github.com/juank27ra' target='_blanc'><img title='Github' className='h-12 w-12 sm:h-9 sm:w-9  drop-shadow-lg hover:shadow-gray-300 hover:h-20 hover:w-20 sm:hover:h-16 sm:hover:w-16' src={github} alt='icono de github'/></a> 
+          <a href='https://github.com/juank27ra' target='_blanc'><img title='Github' className='h-12 w-12 sm:h-9 sm:w-9  drop-shadow-lg hover:shadow-gray-300 hover:h-20 hover:w-20 sm:hover:h-16 sm:hover:w-16' src={Github} alt='icono de github'/></a> 
           <a href='mailto: juank27ra@gmail.com' target='_blanc'><img title='Gmail' className='h-12 w-12 sm:h-9 sm:w-9 drop-shadow-lg hover:shadow-red-600 hover:h-20 hover:w-20 sm:hover:h-16 sm:hover:w-16' src={mail} alt='icono de mail'/></a> 
           <a href='https://wa.me/573224772180' target='_blanc'><img title='Whatsapp' className='h-12 w-12 sm:h-9 sm:w-9 drop-shadow-lg hover:shadow-green-600 hover:h-20 hover:w-20 sm:hover:h-16 sm:hover:w-16' src={whatsapp} alt='icono de whatsap'/></a>  
         </section>
